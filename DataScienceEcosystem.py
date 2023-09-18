0b3d3179fce327d3c70a8dc5224dc6b790993cc7
edgarl33 <78195931+edgarl33@users.noreply.github.com>
2023-09-18 14:55:40 -0600

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# **Objectives:**
+# 
+# * List of popular languages in Data Sciences
+# * Libraries used by Data Scientist
+# * Table of development enviroments for Data scientist
+# * Examples of mathematical operations in Python 
+# 
+# ## Author
+# Edgar Gabriel Lee Romero
+
+# Some of the popular languages that Data Scientist use are:
+# 
+# 1. Python
+# 2. Julia
+# 3. R
+# 4. Ruby
+# 5. Scala
+
+# Some of the commonly use libraries used by Data Scientist include: 
+# 
+# 1. Pytorch
+# 2. Tenserflow
+# 3. Pandas
+# 4. Nunpy
+# 
+
+# |Data Science tools|
+# |------------------|
+# |Jupyter Notebook|
+# |VS Code|
+# |Spider|
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[3]:
+
+
+#This a simple arithmetic expression to multiply then add integers
+(3*4)+5
+
+
+# In[4]:
+
+
+#This will convert 200 minutes to hours by dividing by 60
+200/60
+
+
+# 
+# 
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
